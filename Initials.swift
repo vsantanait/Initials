4ed511662f49ea77841a19882fa03bdbc5459d09
Vanessa Santana <vsantana.it@gmail.com>
2021-05-22 21:36:18 -0400

    Create Initials.swift

---
+++ Initials.swift
@@ -0,0 +1,9 @@
+// Fun Fact: My name is Vanessa. I am learning Latin!
+print("V   V  SSS ")
+print("V   V S   S")
+print("V   V S    ")
+print("V   V  SSS ")
+print("V   V     S")
+print(" V V  S   S")
+print("  V    SSS ")
+
